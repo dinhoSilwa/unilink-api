a303d25cfb95d939f131f517d265ae680f6fc732
claudio silva <claudiosilva.one@gmail.com>
2024-10-01 08:44:12 -0300

    feat : adds Types

--- src/controller/authController/index.ts
+++ src/controller/authController/index.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import { AuthService } from "../../service/auth";
+import type { Iauthcredentials } from "../../types/auth";
 
 export class AuthController {
   static async registerUserAuth(req: Request, res: Response): Promise<void> {
@@ -9,6 +10,16 @@ export class AuthController {
     } catch (error) {
       console.error(error);
       res.status(500).json(error);
+    }
+  }
+
+  static async authcredentials(req: Request, res: Response): Promise<void> {
+    try {
+      const userAuth = await AuthService.authcredentials(req.body);
+      res.status(200).json({ userAuth });
+    } catch (error) {
+      console.error(error);
+      res.status(404).json({ msg: "Usuário não Encontrado", error: error });
     }
   }
 }
