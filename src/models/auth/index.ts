2d379e644c1b792d2e3186e50de5aa2627e46fed
claudio silva <claudiosilva.one@gmail.com>
2024-10-01 08:42:16 -0300

    feat : adds types and models

--- src/models/auth/index.ts
+++ src/models/auth/index.ts
@@ -1,13 +1,15 @@
 import mongoose, { Schema } from "mongoose";
+import type { Iauth } from "../../types/auth";
 
 export const authSchema = new Schema(
   {
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true},
   },
   {
     timestamps: true,
   }
 );
 
-export const UserAuthModel = mongoose.model("User", authSchema);
+export const UserAuthModel = mongoose.model<Iauth>("User", authSchema);
--- src/types/auth/index.ts
+++ src/types/auth/index.ts
@@ -1,7 +1,13 @@
 import { Document } from "mongoose";
 export interface Iauth extends Document {
+  name: string;
   email: string;
   password: string;
   createdAt?: Date;
   updatedAt?: Date;
+}
+
+export interface Iauthcredentials {
+  email: string;
+  password: string;
 }
