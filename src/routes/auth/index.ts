08b35f1db4d4e407619d39f134578acf4df4688a
claudio silva <claudiosilva.one@gmail.com>
2024-10-01 08:43:01 -0300

    feat : adds routers configs

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -2,4 +2,5 @@ import { Router } from "express";
 import { AuthController } from "../../controller/authController";
 
 export const authRouter = Router();
-authRouter.post("/", AuthController.registerUserAuth);
+authRouter.post("/register", AuthController.registerUserAuth);
+authRouter.post("/login", AuthController.authcredentials);
