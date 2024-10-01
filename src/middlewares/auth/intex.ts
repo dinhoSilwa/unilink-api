568dda53071857995e63afb1d64a43b15e696e8f
claudio silva <claudiosilva.one@gmail.com>
2024-10-01 08:43:25 -0300

    feat : adds authentication jwt token

---
+++ src/middlewares/auth/intex.ts
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import { configDotenv } from "dotenv";
+import type { NextFunction, Response, Request } from "express";
+
+configDotenv();
+
+const secret = process.env.JWTSECRET;
+
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers["authorization"]?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ msg: "Token Não Fornecido" });
+  }
+
+  jwt.verify(token, secret!, (err, user) => {
+    if (err) {
+      return res.status(403).json({ msg: "Token Inválido" });
+    }
+    req.user = user;
+    next();
+  });
+};
