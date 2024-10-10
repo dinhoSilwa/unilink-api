b9fb23696b30befa5a647f71d0707dab8c934621
claudio silva <claudiosilva.one@gmail.com>
2024-10-10 15:01:52 -0300

    fix-refactor

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,10 @@ import express, { Application } from "express";
 import cors from "cors";
 import { authRouter } from "./routes/auth";
 import { userRourter } from "./routes/user";
-import { serverMiddlewares } from "./middlewares/serverMiddleware";
+//import { serverMiddlewares } from "./middlewares/serverMiddleware";
 export const app: Application = express();
 
-serverMiddlewares(app);
+//serverMiddlewares(app);
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRourter);
 
